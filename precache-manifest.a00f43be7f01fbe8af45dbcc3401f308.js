ef0e861af7805f7913e3beb5c357ba5e93760bbc
Doyage <Doyage@users.noreply.github.com>
2021-11-17 14:19:12 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.a00f43be7f01fbe8af45dbcc3401f308.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210ae755ae496dd0f9332ccf3449b87d",
+    "url": "https://Doyage.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "fdace06e0c833c8fa136",
+    "url": "https://Doyage.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "37fd3c6ca07f61ca0ba7",
+    "url": "https://Doyage.github.io/task-management-frontend/static/js/2.ded7313c.chunk.js"
+  },
+  {
+    "revision": "fdace06e0c833c8fa136",
+    "url": "https://Doyage.github.io/task-management-frontend/static/js/main.d617bac4.chunk.js"
+  },
+  {
+    "revision": "bf8fbccd51a3c2329735",
+    "url": "https://Doyage.github.io/task-management-frontend/static/js/runtime~main.4eb32413.js"
+  }
+]);
